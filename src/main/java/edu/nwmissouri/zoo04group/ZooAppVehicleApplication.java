713739b4b7b5801aa69bea3f24b8dd01b45f0d2c
REHAMAN NAGURU ABDUR <37996078+nagururehaman@users.noreply.github.com>
2021-11-01 12:39:25 -0500

    Added Spring Initializr files to the ZooAppVehicle

---
+++ src/main/java/edu/nwmissouri/zoo04group/ZooAppVehicleApplication.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo04group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooAppVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooAppVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nwmissouri/zoo04group/ZooAppVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.zoo04group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooAppVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

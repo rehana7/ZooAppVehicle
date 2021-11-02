3d43d9daaf82f5115423e3f4f742ab29b328bde0
rehana7 <89412687+rehana7@users.noreply.github.com>
2021-11-02 11:45:00 -0500

    adding Animal and AnimalGender classes

---
+++ src/main/java/edu/nwmissouri/zoo04group/AnimalGender.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.nwmissouri.zoo04group;
+
+/**
+ *
+ * @author Rehana Naguru 
+ * 
+ * enumerated type for gender
+ */
+ 
+public enum AnimalGender {
+    FEMALE,
+    MALE,
+    UNKNOWN
+}
